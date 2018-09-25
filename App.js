ec3815e42a3e22c5d5ac209e802a9d64ef5f2f8c
arrBaltazar <bprogramming47@gmail.com>
2018-09-24 21:59:18 -0700

    Added a header section and changed the background colors to be more appealing and look clean

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import { StyleSheet, Text, View,TextInput } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
+     
       <View style={styles.container}>
-       <Text style ={styles.text}>What good things happened today?</Text>
-       <TextInput style={styles.input} placeholder='good thing'></TextInput>
-       <TextInput style={styles.input} placeholder='good thing'></TextInput>
-       <TextInput style={styles.input} placeholder='good thing'></TextInput>
-       <Text style ={styles.text}>What could have gone better?</Text>
-       <TextInput style={styles.input}placeholder='Improvements'></TextInput>
+       <View style={styles.header}>
+        <Text style={styles.headerText}>Header Section</Text>
+       </View>
+        <View style={styles.centering}>
+          <Text style ={styles.text}>What good things happened today?</Text>
+          <TextInput style={styles.input} placeholder='good thing'></TextInput>
+          <TextInput style={styles.input} placeholder='good thing'></TextInput>
+          <TextInput style={styles.input} placeholder='good thing'></TextInput>
+          <Text style ={styles.text}>What could have gone better?</Text>
+          <TextInput style={styles.input}placeholder='Improvements'></TextInput>
+        </View>
       </View>
     );
   }
@@ -19,10 +25,29 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#004080',
+    backgroundColor:'#cce6ff',
+    
+  },
+
+   centering:{
     alignItems: 'center',
     justifyContent: 'center',
     padding:16,
+   },
+
+  header:{
+    backgroundColor: '#ccffcc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: '#ddd',
+    marginTop:30,
+  },
+
+  headerText:{
+    color:'#3399ff',
+    fontSize:18,
+    padding:26,
+
   },
 
   text: {
